4abb0ebf6619e08c84a1c73df773c6308bff4d58
Rovert515 <trevorjohnson515@gmail.com>
2023-11-19 17:19:19 +0100

    Added headbob

--- Assets/Scripts/Character Controller/CharacterMotor.cs
+++ Assets/Scripts/Character Controller/CharacterMotor.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.Events;
 
-// TODO make sprinting actually do something, set up a step coroutine that can be activated when you start moving and deactivate when you stop (just have the
-  // interval be based on whether you're sprinting or not when you step, we don't need to change mid stride), test stepping and sprinting, make it so you de-celerate
-  // when you stop sprinting but keep moving, add listeners so stepping has head bob and sound effect
-
 // modified version of Benno's first person controller from the first person labs
 
 // Require a character controller to be attached to the same game object
@@ -23,11 +19,11 @@ public class CharacterMotor : MonoBehaviour
 	public CharacterMotorMovingPlatform movingPlatform = new CharacterMotorMovingPlatform ();
 	public CharacterMotorSliding sliding = new CharacterMotorSliding ();
 
-	// For the next variables, [HideInInspector] tells Unity to not serialize the variable or show it in the inspector view.
-	// Very handy for organization!
+    // For the next variables, [HideInInspector] tells Unity to not serialize the variable or show it in the inspector view.
+    // Very handy for organization!
 
-	// The current global direction we want the character to move in.
-	[HideInInspector]
+    // The current global direction we want the character to move in.
+    [HideInInspector]
 	public Vector3 inputMoveDirection = Vector3.zero;
 
 	// Is the jump button held down? We use this interface instead of checking
@@ -37,9 +33,6 @@ public class CharacterMotor : MonoBehaviour
 
 	[HideInInspector]
 	public bool grounded = true;
-
-	[HideInInspector]
-	public bool sprinting = false;
 
 	[HideInInspector]
 	public Vector3 groundNormal = Vector3.zero;
@@ -53,12 +46,9 @@ public class CharacterMotor : MonoBehaviour
 
 	[SerializeField]
 	private float strideLength = 1.0f; // time between footsteps while walking
-	[SerializeField]
-	public float sprintLength = 0.25f; // time between footsteps while running
 
 	[HideInInspector]
-    public UnityEvent stepEvent; // a Unity event that is invoked when the player takes a step (when they move continuously for strideLength, or sprintLength
-	   // if they're running)
+    public UnityEvent stepEvent; // a Unity event that is invoked when the player takes a step
 
 	private void Test()
 	{
@@ -72,9 +62,6 @@ public class CharacterMotor : MonoBehaviour
 		public float maxForwardSpeed = 10.0f;
 		public float maxSidewaysSpeed = 10.0f;
 		public float maxBackwardsSpeed = 10.0f;
-
-		// The amount to multiply horizontal speed and ground acceleration by when running
-		public float sprintMultiplier = 2.0f;
 	
 		// Curve for multiplying speed based on slope (negative = downwards)
 		public AnimationCurve slopeSpeedMultiplier = new AnimationCurve (new Keyframe (-90, 1), new Keyframe (0, 1), new Keyframe (90, 0));
@@ -228,15 +215,6 @@ public class CharacterMotor : MonoBehaviour
 	{
 		// We copy the actual velocity into a temporary variable that we can manipulate.
 		Vector3 velocity = movement.velocity;
-
-		// We update whether the player is running or not
-		if (Input.GetKey(KeyCode.LeftShift) | Input.GetKey(KeyCode.RightShift))
-		{
-			sprinting = true;
-		} else
-		{
-			sprinting = false;
-		}
 	
 		// Update velocity based on input
 		velocity = ApplyInputVelocityChange (velocity);
@@ -357,6 +335,8 @@ public class CharacterMotor : MonoBehaviour
 			movingPlatform.activeGlobalRotation = tr.rotation;
 			movingPlatform.activeLocalRotation = Quaternion.Inverse (movingPlatform.activePlatform.rotation) * movingPlatform.activeGlobalRotation; 
 		}
+
+
 	}
 
 	void FixedUpdate ()
