248299b58ac812f1363de8a4c23d1d0a09082a0d
CassidyOlson <cassidy.m.olson@gmail.com>
2023-11-28 13:23:22 +0100

    Pause Menu basically working

--- Assets/Scripts/Pause_Menu_Manager.cs
+++ Assets/Scripts/Pause_Menu_Manager.cs
@@ -7,22 +7,33 @@ public class Pause_Menu_Manager : MonoBehaviour
 {
 
     private GameObject PauseMenu;
+    private GameObject HelpMenu;
+    private GameObject SettingsMenu;
     private GameObject Player;
 
     private Assets.Pixelation.Scripts.Chunky chunkyScript;
 
     private bool isPaused;
+    private bool menuOpen;
 
     // Start is called before the first frame update
     void Start()
     {
         PauseMenu = GameObject.FindWithTag("PauseMenu");
         PauseMenu.SetActive(false);
+
+        HelpMenu = GameObject.FindWithTag("HelpMenu");
+        HelpMenu.SetActive(false);
+
+        SettingsMenu = GameObject.FindWithTag("SettingsMenu");
+        SettingsMenu.SetActive(false);
+
         Player = GameObject.FindWithTag("Player");
         chunkyScript = Player.GetComponentInChildren<Assets.Pixelation.Scripts.Chunky>(true);
         chunkyScript.enabled = false;
 
         isPaused = false;
+        menuOpen = false;
     }
 
     // Update is called once per frame
@@ -31,31 +42,77 @@ public class Pause_Menu_Manager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Escape) == true)
         {
-            isPaused = !isPaused;
-        }
+            if (menuOpen == false)
+            {
+                SwapPaused();
+            }
 
-        if (isPaused == true)
-        {
-            PauseGame();
-            PauseMenu.SetActive(true);
-            chunkyScript.enabled = true;
-
-        }
-        else //isPaused = false
-        {
-            ResumeGame();
-            PauseMenu.SetActive(false);
-            chunkyScript.enabled = false;
+            if (menuOpen == false)
+            {
+                if (isPaused == false)
+                {
+                    ResumeGame();
+                }
+                else //isPaused true
+                {
+                    PauseGame();
+                }
+            }
+            
         }
     }
 
     //Pause game
+    public void SwapPaused()
+    {
+        isPaused = !isPaused;
+    }
     void PauseGame()
     {
         Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+        chunkyScript.enabled = true;
+        isPaused = true;
     }
-    void ResumeGame()
+    public void ResumeGame()
     {
         Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+        chunkyScript.enabled = false;
+        isPaused = false;
+    }
+
+    //Open help menu
+    public void OpenHelp()
+    {
+        menuOpen = true;
+        PauseMenu.SetActive(false);
+        HelpMenu.SetActive(true);
+    }
+    public void CloseHelp()
+    {
+        menuOpen = false;
+        HelpMenu.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    //Open settings menu
+    public void OpenSettings()
+    {
+        menuOpen = true;
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        menuOpen = false;
+        SettingsMenu.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    //Quit game
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
