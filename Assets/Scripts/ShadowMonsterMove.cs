681b034323ea2ddc7f6d07404593c120159b2302
TABIBeck <becktiago98@gmail.com>
2023-12-01 15:27:43 +0100

    Quick backup

--- Assets/Scripts/ShadowMonsterMove.cs
+++ Assets/Scripts/ShadowMonsterMove.cs
@@ -5,12 +5,20 @@ using UnityEngine.AI;
 
 public class ShadowMonsterMove : MonoBehaviour
 {
-    NavMeshAgent agent; // the shadow monster's navmesh
-    Transform playerPos; // the position of the player, given to it by the collider that triggers it
+    private NavMeshAgent agent; // the shadow monster's navmesh
+    private Transform playerPos; // the position of the player, given to it by the collider that triggers it
+
+    private SkinnedMeshRenderer[] meshes; // the meshes that make up this ShadowMonster
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        meshes = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+    public void ReceivePlayerPos(Transform player) // just a write only accessor to playerPos for ShadowSpawn to give the player position to
+    {
+        playerPos = player;
     }
 
     // Update is called once per frame
@@ -26,12 +34,20 @@ public class ShadowMonsterMove : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player")) // if it's run into the player
         {
-            Death();
+            PlayerDeath();
         }
     }
 
-    private void Death()
+    private void PlayerDeath()
     {
         // YOU DIED!
+    }
+
+    public void Illuminated() // this monster is illuminated, and so it dies
+    {
+        foreach (SkinnedMeshRenderer mesh in meshes)
+        {
+            mesh.material.color = Color.red; // temp code
+        }
     }
 }
--- Assets/Scripts/ShadowSpawn.cs
+++ Assets/Scripts/ShadowSpawn.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // pretty simple script, just grabs and deactivates anything it has as children at the start (presumably a shadow monster),
-// then reactivates it when the player enters its collider zone. Only activates once
+// then reactivates it when the player enters its collider zone. Only activates once. For all of its shadow monster children it
+// gives them the player's transform when it activates them
 public class ShadowSpawn : MonoBehaviour
 {
     bool triggered = false;
+    ShadowMonsterMove[] shadowChildren = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        shadowChildren = GetComponentsInChildren<ShadowMonsterMove>(); // collects all the shadow monsters from the children
         foreach (Transform child in transform)
         {
             child.gameObject.SetActive(false);
@@ -24,6 +27,10 @@ public class ShadowSpawn : MonoBehaviour
             foreach (Transform child in transform)
             {
                 child.gameObject.SetActive(true);
+            }
+            foreach (ShadowMonsterMove shadow in shadowChildren) // gives all the child shadow monster's the player's position
+            {
+                shadow.ReceivePlayerPos(other.transform);
             }
             triggered = true;
         }
