bdf972c39c62c8c1f44f095f02727611f68b2f4c
CassidyOlson <cassidy.m.olson@gmail.com>
2023-12-09 17:41:56 +0100

    Added shadow dialogue to kill shadow

--- Assets/Scripts/Dialogue_Repository.cs
+++ Assets/Scripts/Dialogue_Repository.cs
@@ -43,40 +43,6 @@ public class Dialogue_Repository : MonoBehaviour
             )
         );
 
-    //After scaring away shadow dialogue
-    static DialogueManager.DialogueState scaredShadowDialogue1 =
-        new DialogueManager.DialogueState("N/A", "",
-            new DialogueManager.DialogueState("That... worked...", "",
-                new DialogueManager.DialogueState("What *was* that?!", "What was what?",
-                    new DialogueManager.DialogueState("The shadow monster!", "...",
-                        new DialogueManager.DialogueState("I'm not crazy.", "I didn't say anything.",
-                            new DialogueManager.DialogueState("Seriously, there was a shadow creature or monster or something!", 
-                                "Okay okay. I mean, look I don't... not believe you?",
-                                new DialogueManager.DialogueState("Really sounds like you do... uh don't.", 
-                                    "I believe you saw something that was scared away by the flashlight. " + 
-                                    "But it probably wasn't a shadow monster.",
-                                    new DialogueManager.DialogueState("Still not sure how I feel about there being something down here with me.", 
-                                        "Can't help you there. Just keep scaring whatever it is away.",
-                                        new DialogueManager.DialogueState("[sarcastic] My hero.")
-                                    )
-                                )
-                            ),
-                            new DialogueManager.DialogueState("Maybe I did hit my head...", 
-                                "There should be a first aid kit somewhere in that bag.",
-                                new DialogueManager.DialogueState("None that I can see.", "Maybe you also lost it in the fall.",
-                                    new DialogueManager.DialogueState("[quietly] Or maybe those shadows took it.", 
-                                        "I didn't quite catch that.",
-                                        new DialogueManager.DialogueState("Nothing nothing, I'll keep an eye out for it.")
-                                    )
-                                )
-                            ),
-                            new DialogueManager.DialogueState("[say nothing]")
-                        )
-                    )
-                )
-            )
-        );
-
     //Arriving at the first puzzle room dialogue
     static DialogueManager.DialogueState puzzleRoom1Dialogue =
         new DialogueManager.DialogueState("N/A", "Okay, there should be a door to the rest of the cave system around here.",
@@ -268,13 +234,6 @@ public class Dialogue_Repository : MonoBehaviour
                 DialogueManager.currentDialogueManager.SetDialogueState(puzzleRoom3Dialogue);
             }
         }
-    }
-    
-    
-    //Trigger for 1st Scaring Away Shadow Dialogue
-    public static void ScareShadowDialogueTrigger1()
-    {
-        DialogueManager.currentDialogueManager.SetDialogueState(scaredShadowDialogue1);
     }
     
     //Trigger for 2nd Scaring Away Shadow Dialogue
