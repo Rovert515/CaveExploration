1b053dbc3122556d03be8a9244a812cfdb60dbc3
CassidyOlson <cassidy.m.olson@gmail.com>
2023-12-09 22:51:36 +0100

    changed little things

--- Assets/Scripts/ShadowSighting.cs
+++ Assets/Scripts/ShadowSighting.cs
@@ -11,7 +11,7 @@ public class ShadowSighting : MonoBehaviour
     static DialogueManager.DialogueState shadowSightingDialogue =
         new DialogueManager.DialogueState("N/A", "",
             new DialogueManager.DialogueState("What the hell was that?!", "What? Is something wrong?",
-                new DialogueManager.DialogueState("I swear I just one of the shadows move.",
+                new DialogueManager.DialogueState("I swear I just saw one of the shadows move.",
                     "It's probabably just your mind playing tricks on you.",
                     new DialogueManager.DialogueState("I... okay.", "Shine your flashlight at it, maybe?")
                 )
